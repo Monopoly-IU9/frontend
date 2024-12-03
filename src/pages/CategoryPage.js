9b97efe18b08fea45cb7231c6e38b4eaee32c759
alex <volohovaleksandr1974@gmail.com>
2024-12-03 15:01:00 +0300

    обновление дизайна страницы категории, часть 1

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -82,9 +82,28 @@ function CategoryPage() {
     };
 
     return (
-        <div className="container my-4 bg-light rounded p-4 shadow-sm">
-            <Link to="/admin/categories" className="btn btn-outline-secondary mb-3">Назад</Link>
-            <h1 className="mb-4">Изменить категорию</h1>
+        <div className="container bg-light rounded p-4 shadow-sm">
+            {/* Кнопка "Назад" */}
+            <Link
+                to="/admin/categories"
+                className="text-decoration-none d-flex align-items-center mb-3"
+                style={{ color: '#0d6efd' }}
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-arrow-left me-1"
+                    viewBox="0 0 16 16"
+                >
+                    <path
+                        fillRule="evenodd"
+                        d="M15 8a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 0 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 0 1 .708.708L2.707 7.5H14.5A.5.5 0 0 1 15 8z"
+                    />
+                </svg>
+                Категории
+            </Link>
 
             {/* Название и цвет */}
             <div className="mb-3">
