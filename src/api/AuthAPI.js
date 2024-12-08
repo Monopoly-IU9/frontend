a8bdb32043a58b2abd05efe8c574dd7c58f8c34b
alex <volohovaleksandr1974@gmail.com>
2024-12-08 03:41:32 +0300

    Исправил конфиг

--- src/api/AuthAPI.js
+++ src/api/AuthAPI.js
@@ -1,22 +1,6 @@
 import axios from 'axios';
 import { BASE_URL } from './config';
 
-// Настраиваем интерсепторы для добавления токена
-axios.interceptors.request.use((config) => {
-    const adminToken = localStorage.getItem('adminToken');
-    const hostToken = localStorage.getItem('hostToken');
-    if (adminToken && config.url.includes('/admin/')) {
-        config.headers.Authorization = `Bearer ${adminToken}`;
-        console.log("В роли администратора");
-        console.log(config.url);
-    } else if (hostToken && config.url.includes('/host/')) {
-        config.headers.Authorization = `Bearer ${hostToken}`;
-        console.log("В роли ведущего");
-        console.log(config.url);
-    }
-    return config;
-});
-
 export const loginAdmin = (login, password) =>
     axios.post(`${BASE_URL}/admin/login`, { login, password });
 
--- src/api/config.js
+++ src/api/config.js
@@ -1,1 +1,19 @@
+import axios from "axios";
+
 export let BASE_URL = 'http://127.0.0.1:8000';
+
+// Настраиваем интерсепторы для добавления токена
+axios.interceptors.request.use((config) => {
+    const adminToken = localStorage.getItem('adminToken');
+    const hostToken = localStorage.getItem('hostToken');
+    if (adminToken && config.url.includes('/admin/')) {
+        config.headers.Authorization = `Bearer ${adminToken}`;
+        console.log("В роли администратора");
+        console.log(config.url);
+    } else if (hostToken && config.url.includes('/host/')) {
+        config.headers.Authorization = `Bearer ${hostToken}`;
+        console.log("В роли ведущего");
+        console.log(config.url);
+    }
+    return config;
+});
